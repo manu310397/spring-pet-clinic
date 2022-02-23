3d5e0a9bad1c7634288268f9097a49820a7c527e
manu310397 <manojb912@gmail.com>
2022-02-23 11:52:17 +0530

    Implement Map based services. Closes #3 Add pet clinic Index page and controller. Closes #4

---
+++ pet-clinic-service/src/main/java/com/manoj/springpetclinic/map/VetServiceMap.java
@@ -0,0 +1,44 @@
+package com.manoj.springpetclinic.map;
+
+import com.manoj.springpetclinic.VetService;
+import com.manoj.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+
+        /*if (object.getSpecialities().size() > 0){
+            object.getSpecialities().forEach(speciality -> {
+                if(speciality.getId() == null){
+                    Speciality savedSpecialty = specialtyService.save(speciality);
+                    speciality.setId(savedSpecialty.getId());
+                }
+            });
+        }*/
+
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
