c42352cbeff80d547ea88aff24937adaa3a3c4be
manu310397 <manojb912@gmail.com>
2022-02-23 10:29:07 +0530

    Refactoring to common interfaces. Closes #2

--- pet-clinic-service/src/main/java/com/manoj/springpetclinic/PetService.java
+++ pet-clinic-service/src/main/java/com/manoj/springpetclinic/PetService.java
@@ -2,12 +2,5 @@ package com.manoj.springpetclinic;
 
 import com.manoj.springpetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
