1dcbee6762795205b3a01fae24e54558ce179258
manu310397 <manojb912@gmail.com>
2022-02-26 09:18:09 +0530

    Load data on bootstrap. closes #5

---
+++ pet-clinic-web/src/main/java/com/manoj/springpetclinic/bootstrap/DataLoader.java
@@ -0,0 +1,62 @@
+package com.manoj.springpetclinic.bootstrap;
+
+import com.manoj.springpetclinic.OwnerService;
+import com.manoj.springpetclinic.VetService;
+import com.manoj.springpetclinic.map.OwnerServiceMap;
+import com.manoj.springpetclinic.map.VetServiceMap;
+import com.manoj.springpetclinic.model.Owner;
+import com.manoj.springpetclinic.model.Vet;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+    private final OwnerService ownerService;
+    private final VetService vetService;
+
+    public DataLoader() {
+        this.ownerService = new OwnerServiceMap();
+        this.vetService = new VetServiceMap();
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
+        owner1.setAddress("123 Brickerel");
+        owner1.setCity("Miami");
+        owner1.setTelephone("1231231234");
+
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+        owner2.setAddress("123 Brickerel");
+        owner2.setCity("Miami");
+        owner2.setTelephone("1231231234");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded owners....");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet2);
+
+        System.out.println("Loaded Vets....");
+    }
+}
