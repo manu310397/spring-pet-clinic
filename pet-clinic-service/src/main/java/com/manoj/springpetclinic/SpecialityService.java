4abe773e3256b3a2f48b0a1c19c417b3df9f5bbd
manu310397 <manojb912@gmail.com>
2022-03-04 15:16:08 +0530

    Close #13, #16

---
+++ pet-clinic-service/src/main/java/com/manoj/springpetclinic/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.manoj.springpetclinic;
+
+import com.manoj.springpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
---
+++ pet-clinic-service/src/main/java/com/manoj/springpetclinic/map/SpecialityServiceMap.java
@@ -0,0 +1,35 @@
+package com.manoj.springpetclinic.map;
+
+import com.manoj.springpetclinic.SpecialityService;
+import com.manoj.springpetclinic.model.Speciality;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
