5f0bf07a52b91caaf17b6a76fd7d50d5cbc8b2b8
manu310397 <manojb912@gmail.com>
2022-03-23 11:43:57 +0530

    Implemented ownerDetails.html

--- pet-clinic-web/src/test/java/com/manoj/springpetclinic/controller/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/manoj/springpetclinic/controller/OwnerControllerTest.java
@@ -14,7 +14,8 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import java.util.HashSet;
 import java.util.Set;
 
-import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -65,4 +66,18 @@ class OwnerControllerTest {
 
         verifyNoInteractions(ownerService);
     }
+
+    @Test
+    void showOwner() throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+
+        when(ownerService.findById(anyLong())).thenReturn(owner1);
+
+        mockMvc.perform(get("/owners/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("owners/ownerDetails"))
+                .andExpect(model().attribute("owner", hasProperty("id", is(1L))));
+    }
+
 }
